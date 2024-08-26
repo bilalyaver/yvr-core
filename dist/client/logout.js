d64e04029a5f821846bf5109783df662552c40bb
Bilal Yaver <bilalyaver@outlook.com>
2024-08-26 10:11:14 +0300

    chore: Remove unused files and code

--- dist/client/logout.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logout = void 0;
-const cookies_next_1 = require("cookies-next");
-const logout = async () => {
-    (0, cookies_next_1.deleteCookie)('token');
-    window.location.href = '/login';
-};
-exports.logout = logout;
