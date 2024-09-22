b446d01fb32b49d80ac42253a5f6ca6815b4dc62
Bilal Yaver <bilalyaver@outlook.com>
2024-09-22 09:30:11 +0300

    chore: Bump package version to 0.4.0 and update config file path to yvr.config.json

--- server/config/loadConfigFromFile.ts
+++ server/config/loadConfigFromFile.ts
@@ -4,7 +4,7 @@ import path from 'path';
 // Config dosyasını yükle
 export function loadConfigFromFile(): any {
   // Projenin çalışma dizinine (root) göre config dosyasının yolunu belirler
-  const configPath = path.join(process.cwd(), 'settings.json');  // Root'ta config.json dosyasını arar
+  const configPath = path.join(process.cwd(), 'yvr.config.json');  // Root'ta config.json dosyasını arar
 
   try {
     const configData = fs.readFileSync(configPath, 'utf-8');
